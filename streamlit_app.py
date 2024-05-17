46f9619fac07b8c85481f191dd07545306ac205e
Abdelkadeer <90860614+Abdelkadeer@users.noreply.github.com>
2024-05-17 20:55:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,21 @@
 import streamlit as st
-st.write('Torkhani Abdelkader')
+import pandas as pd
+from keybert import KeyBERT
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+import yake
+st.markdown("# Une approche hybride pour l’analyse des documents médicaux")
+# Chargement des données
+data = pd.read_csv('mtsamples.csv')
+data.rename(columns={"Unnamed: 0": "ID"}, inplace=True)
+colonnes_a_visualiser = ['ID', 'description', 'medical_specialty', 'sample_name', 'transcription', 'keywords']
+data_selection = data[colonnes_a_visualiser]
+
+# Afficher les données
+st.title('Données Médicales : Extraction des mots clés')
+
+# Sélection des colonnes à afficher
+colonnes_selectionnees = st.multiselect('Sélectionner les colonnes à visualiser', colonnes_a_visualiser)
+
+# Filtrer les données en fonction des colonnes sélectionnées
+data_affichee = data_selection[colonnes_selectionnees]
