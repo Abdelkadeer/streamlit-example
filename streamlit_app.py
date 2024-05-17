0d01f36c1626f40c7d041d3fa89d747fc1700a70
Abdelkadeer <90860614+Abdelkadeer@users.noreply.github.com>
2024-05-17 21:52:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
 st.markdown("# Une approche hybride pour l’analyse des documents médicaux")
 # Chargement des données
 data = pd.read_csv('mtsamples.csv')
@@ -17,3 +17,17 @@ colonnes_selectionnees = st.multiselect('Sélectionner les colonnes à visualise
 # Filtrer les données en fonction des colonnes sélectionnées
 data_affichee = data_selection[colonnes_selectionnees]
 st.write(data_affichee)
+# Compter le nombre de valeurs manquantes pour chaque colonne sélectionnée
+missing_values = data_selection.isnull().sum()
+
+# Créer un graphique à barres pour visualiser les valeurs manquantes
+fig, ax = plt.subplots(figsize=(10, 6))
+missing_values.plot(kind='bar', color='skyblue', ax=ax)
+plt.title('Nombre de valeurs manquantes par colonne')
+plt.xlabel('Colonnes')
+plt.ylabel('Nombre de valeurs manquantes')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+
+# Afficher le graphique avec Streamlit
+st.pyplot(fig)
